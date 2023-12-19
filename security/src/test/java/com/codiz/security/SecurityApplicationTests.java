fb33ba32ddefa76dcd429123fe44f80e76b1e746
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2023-12-19 16:40:02 +0300

    security learnig

---
+++ security/src/test/java/com/codiz/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codiz.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
